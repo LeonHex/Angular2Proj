7140db5eea585a4ab4dec909ed5067d889a484ea
guang yang <blasttnt@gmail.com>
2017-04-17 18:48:31 +0800

    upd_d1_data-binding

--- hero-tutorial/src/app/app.component.js
+++ hero-tutorial/src/app/app.component.js
@@ -8,14 +8,16 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
-        this.name = 'Angular';
+        this.name = 'Guang';
+        this.title = 'Heroes list';
+        this.hero = 'Windstorm';
     }
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "<h1>Hello {{name}}</h1>",
+        template: "<h1>{{title}}</h1><h2>{{hero}} details!",
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
