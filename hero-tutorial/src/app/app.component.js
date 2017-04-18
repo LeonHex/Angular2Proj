4a2e0712b0aa2c7b51bfa4f55e71d2989ec90e2e
guang yang <blasttnt@gmail.com>
2017-04-18 17:19:50 +0800

    upd_hero_list_added upd_external_css_file_added

--- hero-tutorial/src/app/app.component.js
+++ hero-tutorial/src/app/app.component.js
@@ -14,13 +14,25 @@ var AppComponent = (function () {
         this.name = 'Leon';
         this.title = 'Tour of Heroes';
         this.hero = new Hero(1, 'Windstorm');
-        this.heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+        this.heroes = [
+            { id: 11, name: 'Mr. Nice' },
+            { id: 12, name: 'Narco' },
+            { id: 13, name: 'Bombasto' },
+            { id: 14, name: 'Celeritas' },
+            { id: 15, name: 'Magneta' },
+            { id: 16, name: 'RubberMan' },
+            { id: 17, name: 'Dynama' },
+            { id: 18, name: 'Dr IQ' },
+            { id: 19, name: 'Magma' },
+            { id: 20, name: 'Tornado' }
+        ];
     }
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
+        styleUrls: ['./app.component.css'],
         templateUrl: './app.component.html',
     }),
     __metadata("design:paramtypes", [])
--- hero-tutorial/src/app/app.component.ts
+++ hero-tutorial/src/app/app.component.ts
@@ -2,19 +2,32 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
+  styleUrls: ['./app.component.css'],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
   name: string;
   title: string;
   hero: Hero;
-  heroes: Array<string>;
+  heroes: Array<Hero>;
+
 
   constructor() {
     this.name = 'Leon';
     this.title = 'Tour of Heroes';
-    this.hero = new Hero(1,'Windstorm');
-    this.heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+    this.hero = new Hero(1, 'Windstorm');
+    this.heroes = [
+      { id: 11, name: 'Mr. Nice' },
+      { id: 12, name: 'Narco' },
+      { id: 13, name: 'Bombasto' },
+      { id: 14, name: 'Celeritas' },
+      { id: 15, name: 'Magneta' },
+      { id: 16, name: 'RubberMan' },
+      { id: 17, name: 'Dynama' },
+      { id: 18, name: 'Dr IQ' },
+      { id: 19, name: 'Magma' },
+      { id: 20, name: 'Tornado' }
+    ];
   }
 }
 
