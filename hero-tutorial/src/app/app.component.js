220448450ff7ae8ebae767d38803dfe2497fb57b
guang yang <blasttnt@gmail.com>
2017-04-26 17:51:03 +0800

    update_router_mechanism_complete

--- hero-tutorial/src/app/app.component.js
+++ hero-tutorial/src/app/app.component.js
@@ -18,7 +18,8 @@ var AppComponent = (function () {
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        templateUrl: './app.component.html'
+        templateUrl: './app.component.html',
+        styleUrls: ['./app.component.css']
     }),
     __metadata("design:paramtypes", [])
 ], AppComponent);
--- hero-tutorial/src/app/hero-detail.component.ts
+++ hero-tutorial/src/app/hero-detail.component.ts
@@ -8,7 +8,8 @@ import { HeroService } from './hero.service';
 
 @Component({
     selector: 'hero-detail',
-    templateUrl: './hero-detail.component.html'
+    templateUrl: './hero-detail.component.html',
+    styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
     @Input() hero: Hero;
@@ -17,8 +18,8 @@ export class HeroDetailComponent implements OnInit {
 
     ngOnInit() {
         this.route.params
-        .switchMap((params: Params) => this.heroService.getHero(+params['id']))
-        .subscribe(hero => this.hero = hero);
+            .switchMap((params: Params) => this.heroService.getHero(+params['id']))
+            .subscribe(hero => this.hero = hero);
     }
 
     goBack(): void {
